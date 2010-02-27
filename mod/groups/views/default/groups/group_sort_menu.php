eb45a899f910a6e73e8e93f26ca2fb63ce849604
joebo <jbogner@dreamfish.com>
2010-02-27 14:01:12 -0800

    group page content was showing on the project page

--- mod/groups/views/default/groups/group_sort_menu.php
+++ mod/groups/views/default/groups/group_sort_menu.php
@@ -13,13 +13,7 @@
 	 //url
 	 $url = $vars['url'] . "pg/groups/world/";
 
-	$pages = search_for_object('DF_'.'Starting_A_Group');
-	if ($pages && sizeof($pages) > 0) {
-		$toptext .= $pages[0]->description;
-	} 
-
 ?>
-<?php echo $toptext ?>
 <div id="elgg_horizontal_tabbed_nav">
 <ul>
 	<li <?php if($filter == "newest") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=newest"><?php echo elgg_echo('groups:newest'); ?></a></li>
