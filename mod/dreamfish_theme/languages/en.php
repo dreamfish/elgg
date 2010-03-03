f0d2c46835d7f60fbe2fdee769e32af0986e3ace
Fablife <fbarone@dreamfish.com>
2010-03-03 23:49:21 +0100

    Added terms and guidelines text and confirmation checkbox to registration page

---
+++ mod/dreamfish_theme/languages/en.php
@@ -0,0 +1,13 @@
+<?php
+ 
+  $english = array(	
+	/* added by Fabio Barone: accept membership guidelines and terms of membership */
+        'dreamfish_theme:accept_terms' => "By checking the box below, I declare that I have<br>
+read and accept the <a href=\"http://network.dreamfish.com/elgg/pg/pages/view/205/\">Dreamfish membership guidelines</a><br> and the <a href=\"http://network.dreamfish.com/elgg/pg/pages/view/204/\">Terms of membership</a>.",
+                        'yes_dreamfish' => "Yes, I want to Dreamfish!",
+        'dreamfish_theme:join' => "Join",
+	);
+ 
+  add_translation("en",$english);
+ 
+?>
