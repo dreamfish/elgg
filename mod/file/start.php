d3ced8ac348288032924cc18e774ffaa189406ce
joebo <jbogner@dreamfish.com>
2010-03-19 06:12:39 -0700

    add pages/files to groups

--- mod/file/start.php
+++ mod/file/start.php
@@ -47,6 +47,8 @@
 		// Extend hover-over and profile menu	
 	    extend_view('profile/menu/links','file/menu');
 		extend_view('groups/left_column','file/groupprofile_files');
+		extend_view('dgroups/left_column','file/groupprofile_files');
+
 		
 		// Register a page handler, so we can have nice URLs
 		register_page_handler('file','file_page_handler');
